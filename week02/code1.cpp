cacbef2a583c1d538ddfab0b2e6aef6d6bca3a48
Gabe de la Cruz <gvdelacruz@nic.edu>
2020-08-31 10:58:14 -0700

    add lecture code

---
+++ week02/code1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    // cout << "Hello, " << "World!\n";
+    // cout << "Hello, " 
+    //      << "World!\n";
+    // cout << "Hello, ";
+    // cout << "World!\n";
+
+    cout << "\"Hi\tthere\"\n";
+
+    int age = 17;
+
+    cout << "My age is: " << age << endl;
+    age = 18;
+    cout << "My new age is: " << age << endl;
+
+    int num1, num2, num3;
+    int num4;
+    int num5;
+    int num6;
+    int num7,
+        num8,
+        num9;
+
+    double someNumber = 2;
+    int someNumber2 = 2.3;
+
+    cout << "someNumber2 = " << someNumber2 << endl;
+
+    return 0;
+}
