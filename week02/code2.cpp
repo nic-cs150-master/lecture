21c2411648f3ae9460bc6a4b9f4863e66f869cca
Gabe de la Cruz <gvdelacruz@nic.edu>
2020-09-02 02:57:22 -0700

    code for sec101 added

---
+++ week02/code2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name, address;
+
+    name = "John";
+    address = "Coeur d'Alene";
+
+    cout << "Name: " << name << '\n';
+    cout << "Address: " << address << '\n';
+
+    return 0;
+}
