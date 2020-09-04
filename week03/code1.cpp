546e475fad609804b57f58af437755411ab7c3cf
Gabe de la Cruz <gvdelacruz@nic.edu>
2020-09-04 10:51:41 -0700

    early start on week3

---
+++ week03/code1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length, width;
+
+    // cout << "What is the length of the rectangle? ";
+    // cin >> length;
+
+    // cout << "What is the width of the rectangle? " ;
+    // cin >> width;
+
+    cout << "Enter the length and width of the rectangle: ";
+    cin >> length >> width;
+
+    cout << "length = " << length << '\n';
+    cout << "width = " << width << '\n';
+    return 0;
+}
